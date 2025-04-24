bdd68fbe1f6aacbc9d8907a9ee4a517de7f3973a
Oleksandr Dovhan <70040765+Witchdoctor90@users.noreply.github.com>
2025-04-24 21:45:50 +0300

    Created key UrlEntity

---
+++ UrlShortener.Domain/UrlEntity.cs
@@ -0,0 +1,22 @@
+﻿namespace UrlShortener.Domain;
+
+public class UrlEntity
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    
+    public string LongUrl { get; set; } = string.Empty;
+    public string ShortCode { get; set; } = string.Empty;
+    
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public Guid UserId { get; set; } = Guid.Empty;
+    
+    //Ef constructor
+    private UrlEntity() {}
+
+    public UrlEntity(string longUrl, string shortCode, Guid userId)
+    {
+        LongUrl = longUrl;
+        ShortCode = shortCode;
+        UserId = userId;
+    }
+}
